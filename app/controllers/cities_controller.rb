f8e050ecb9d4b379770fb789456e20c8aa02e88e
Andrew Djoga <andrew.djoga@gmail.com>
2012-01-04 12:28:19 +0200

    Reverse pagination

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,7 +1,7 @@
 class CitiesController < ApplicationController
 
   def index
-    @cities = City.order('created_at DESC').page(params[:page])
+    @cities = prepare_cities City.order('created_at DESC')
   end
 
   def create
@@ -14,6 +14,22 @@ class CitiesController < ApplicationController
     end
 
     redirect_to cities_url
+  end
+
+  private
+
+  def prepare_cities(scope)
+    per_page = City.default_per_page
+    total_count = scope.count
+    rest_count = total_count > per_page ? (total_count % per_page) : 0
+    current_page = (params[:page] || 1).to_i
+    @num_pages = total_count > per_page ? (total_count / per_page) : 1
+
+    if current_page == 1
+      scope.page(1).per(per_page + rest_count)
+    else
+      scope.page(current_page).per(per_page).padding(rest_count)
+    end
   end
 
 end
